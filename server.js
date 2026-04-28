742c73ff62c6f2d5e1bba564d8d107aa9d677b46
akshathakottur-web <akshathakottur@gmail.com>
2026-04-28 19:44:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -321,7 +321,9 @@ app.get("/test", (req, res) => {
 });
 
 // 🚀 START SERVER
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
   console.log("Server running on http://localhost:3000");
 });
 
